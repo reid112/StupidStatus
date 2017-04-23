ea70ea104b062653a75938f3ddbd4f90023111ad
reid112 <rreid112@gmail.com>
2017-04-23 12:55:31 -0600

    init changes

--- app/src/main/java/com/stupidstatus/android/ui/init/InitActivity.java
+++ app/src/main/java/com/stupidstatus/android/ui/init/InitActivity.java
@@ -1,6 +1,7 @@
 package com.stupidstatus.android.ui.init;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
 import com.stupidstatus.android.R;
@@ -14,8 +15,15 @@ public class InitActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(StatusActivity.createIntent(this));
-        finish();
+
+        // Stay on init screen for 1.5 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(StatusActivity.createIntent(InitActivity.this));
+                finish();
+            }
+        }, 1500);
     }
     //endregion
 
